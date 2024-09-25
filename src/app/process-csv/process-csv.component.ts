26533b29d14ad2fccd24ea2fdd7fd37cee80fb30
Andres Pugni <apugni@evoltis.com>
2024-09-25 16:11:59 -0300

    docs

--- src/app/process-csv/process-csv.component.ts
+++ src/app/process-csv/process-csv.component.ts
@@ -29,7 +29,7 @@ export class ProcessCsvComponent {
           latitude: row.LAT,
           longitude: row.LON
         }));
-        console.log('Datos procesados desde el CSV:', data);
+        // console.log('Datos procesados desde el CSV:', data);
         this.csvDataLoaded.emit(data);
       }
 
